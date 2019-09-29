46549349e86c5d0dda4f2ffd07a18a2040f01cb4
Arthur Karganyan <arthur.karganyan@gmail.com>
2019-09-29 07:39:04 +0200

    Added axios

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 import Vue from '/web_modules/vue/dist/vue.esm.browser.js'
 import httpVueLoader from '/web_modules/http-vue-loader/src/httpVueLoader.js'
+import axios from "/web_modules/axios/dist/axios.js"
 
 Vue.use(httpVueLoader);
 
+Vue.prototype.$http = axios;
 new Vue({
   el: '#app',
   components: {
